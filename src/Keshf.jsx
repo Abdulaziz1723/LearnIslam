2aa23dc53382dbd7c950725f212392db4e063d5a
abdulaziz <abdulazizabdu707@gmail.com>
2025-08-07 19:52:48 +0300

    m

--- src/Keshf.jsx
+++ src/Keshf.jsx
@@ -7,5 +7,18 @@ const root = createRoot(document.getElementById('main'));
 
 root.render(
   <StrictMode>
+          <div class="player">
+        <h2>Part:1</h2>
+        <audio id="audio" src="part-11.mp3"></audio>
+      
+        <div class="controls">
+          <button id="toggleBtn">▶️</button>
+          <input type="range" id="seekBar" value="0" step="1" min="0"/>
+        </div>
+      
+        <div class="time">
+          <span id="currentTime">00:00</span> / <span id="duration">00:00</span>
+        </div>
+      </div>
 </StrictMode>
 )
