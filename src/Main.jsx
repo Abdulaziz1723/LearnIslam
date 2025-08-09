80610fcb48bbf8387ba69b782c401d66c7d8035b
abdulaziz <abdulazizabdu707@gmail.com>
2025-08-09 14:39:33 +0300

    commited:

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,6 +3,8 @@ import MainFetch from "./MainFetch"
 export default function Main(){
     return(
         <main>
+        <div id="ayah-widget"></div>
+        <script src="/ayah-widget/ayah.js"></script>
         <MainFetch/>
         </main>
     )
