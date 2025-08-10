f10f120cef6acad869f8188fad470872ce4d2712
abdulaziz <abdulazizabdu707@gmail.com>
2025-08-10 10:52:23 +0300

    comiited

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,3 @@ root.render(
 </StrictMode>
 )
 
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then(registration => {
-      console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch(error => {
-      console.error('Service Worker registration failed:', error);
-    });
-}
