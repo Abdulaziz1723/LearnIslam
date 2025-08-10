5a2943fc426218630401682d507d701c9a10da5f
abdulaziz <abdulazizabdu707@gmail.com>
2025-08-10 10:31:39 +0300

    comiited

--- api/subscribe.js
+++ api/subscribe.js
@@ -2,7 +2,7 @@
 const webpush = require('web-push');
 
 webpush.setVapidDetails(
-  process.env.VAPID_SUBJECT || 'mailto:you@example.com',
+  process.env.VAPID_SUBJECT || 'mailto:abdulazizabdu707@gmail.com',
   process.env.VAPID_PUBLIC_KEY,
   process.env.VAPID_PRIVATE_KEY
 );
